8515792c70ae5cc61d25c5a6345e120f540dad62
Brian Fitzwater - IDI-C <brian.fitzwater@gsa.gov>
2021-02-25 08:27:24 -0500

    Fix standards subscription ARN

--- securityhub.tf
+++ securityhub.tf
@@ -3,7 +3,7 @@ resource "aws_securityhub_account" "account" {
 
 resource "aws_securityhub_standards_subscription" "foundations" {
   depends_on    = [aws_securityhub_account.account]
-  standards_arn = "arn:aws:securityhub:::standards/aws-foundational-security-best-practices/v/1.0.0"
+  standards_arn = "arn:aws:securityhub:us-east-1::standards/aws-foundational-security-best-practices/v/1.0.0"
 }
 
 resource "aws_securityhub_standards_subscription" "cis" {
